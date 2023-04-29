98419116648860e225cedbac9319a030d970cbeb
Ian Heraty <ian.heraty@pm.me>
2023-04-29 11:19:32 -0500

    Add badge color for job listing application status

--- app/helpers/job_listings_helper.rb
+++ app/helpers/job_listings_helper.rb
@@ -1,2 +1,18 @@
 module JobListingsHelper
+  def application_status_color(job_listing)
+    case job_listing.application_status.to_sym
+    when :open
+      "secondary"
+    when :sent
+      "primary"
+    when :interviewing
+      "warning"
+    when :offer
+      "success"
+    when :rejected
+      "danger"
+    else
+      "secondary"
+    end
+  end
 end
