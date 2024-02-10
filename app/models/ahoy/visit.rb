8e3858bddeeb15caa815d19cbc38d714f4672dce
Ian Heraty <ian.heraty@pm.me>
2024-02-11 00:42:09 +0800

    Add after_create callback to trim old visits to stay within heroku limmits

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,6 +1,14 @@
 class Ahoy::Visit < ApplicationRecord
   self.table_name = "ahoy_visits"
 
-  has_many :events, class_name: "Ahoy::Event"
+  has_many :events, class_name: "Ahoy::Event", dependent: :destroy
   belongs_to :user, optional: true
+
+  # TODO: remove after increasing Heroku db plan (issue #29)
+  after_create :trim_old_visits
+
+  def trim_old_visits
+    Ahoy::Visit.excluding(Ahoy::Visit.order(started_at: :desc).limit(3000))
+               .delete_all
+  end
 end
