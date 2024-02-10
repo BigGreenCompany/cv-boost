dc7190d20e92cdc0612bf8203877d80a2a77cf24
Ian Heraty <ian.heraty@pm.me>
2024-02-11 01:04:57 +0800

    Undo trim recent visits. Decided just to upgrade plan

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,14 +1,6 @@
 class Ahoy::Visit < ApplicationRecord
   self.table_name = "ahoy_visits"
 
-  has_many :events, class_name: "Ahoy::Event", dependent: :destroy
+  has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
-
-  # TODO: remove after increasing Heroku db plan (issue #29)
-  after_create :trim_old_visits
-
-  def trim_old_visits
-    Ahoy::Visit.excluding(Ahoy::Visit.order(started_at: :desc).limit(2500))
-               .delete_all
-  end
 end
